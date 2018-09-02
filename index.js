41b3a93a82556db96076a65d9e5b875b0ec7a438
funregis <regis.limare@sfr.fr>
2018-09-02 18:34:55 +0200

    init

--- index.js
+++ index.js
@@ -2,110 +2,130 @@ const Alexa = require('alexa-sdk');
 const request = require('request');
 const {apiKey, appId, serverAddress} = require('./config.json');
 
-const skillName = 'TV Remote';
+const skillName = 'TV Panasonic Remote';
 
-const CODES = {
-  POWER: 'power',
-  NETFLIX: 'netflix',
-  APPS: 'apps',
-  MUTE: 'mute',
-  VOLUME_UP: 'volup,volup,volup,volup,volup',
-  VOLUME_DOWN: 'voldown,voldown,voldown,voldown,voldown',
-  VOLUME_UP_HIGH: 'volup,volup,volup,volup,volup,volup,volup,volup,volup,volup',
-  VOLUME_DOWN_HIGH: 'voldown,voldown,voldown,voldown,voldown,voldown,voldown,voldown,voldown,voldown',
-  PLAY: 'play',
-  PAUSE: 'pause'
+const COMMANDS = {
+  group:{
+    command: 'X_SendKey',
+    url : '/nrc/control_0',
+    urn: 'panasonic-com:service:p00NetworkControl:1'
+  },
+  power : '<X_KeyEvent>NRC_POWER-ONOFF</X_KeyEvent>',
+  volume: {
+    up: '<X_KeyEvent>NRC_VOLUP-ONOFF</X_KeyEvent>',
+    down: '<X_KeyEvent>NRC_VOLDOWN-ONOFF</X_KeyEvent>',
+    mute: '<X_KeyEvent>NRC_MUTE-ONOFF</X_KeyEvent>'
+  },
+  channel: {
+    up: '<X_KeyEvent>NRC_CH_UP-ONOFF</X_KeyEvent>',
+    down: '<X_KeyEvent>NRC_CH_DOWN-ONOFF</X_KeyEvent>'
+  },
+  number: '<X_KeyEvent>NRC_D@input@-ONOFF</X_KeyEvent>'
 };
+const RENDERS = {
+  group:{
+    command : 'SetVolume',
+    url: '/nrc/control_0',
+    urn: 'schemas-upnp-org:service:RenderingControl:1'
+  },
 
-const makeRequest = (command, context) => {
-  request({
-    method: 'GET',
-    uri: `${serverAddress}?command=${command}`,
-    headers: {'Authorization': apiKey},
+  volume: '<InstanceID>0</InstanceID><Channel>Master</Channel><DesiredVolume>@input@</DesiredVolume>'
+} 
+
+const SOAP_TEMPLATES = `<?xml version='1.0' encoding='utf-8'?>
+   <s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/' s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'>
+    <s:Body>
+     <u:@command@ xmlns:u='urn:panasonic-com:service:p00NetworkControl:1'>
+      @action@
+     </u:@command@>
+    </s:Body>
+   </s:Envelope>`;
+   
+const SOAP_ACTION = '"urn:@urn@#@command@"';
+
+const makeRequest = (group, action, context) => {
+  request.post({
+    uri: `${serverAddress}${group.url}`,
+    headers: {'Content-Type': 'text/xml; charset="utf-8', 'SOAPACTION': SOAP_ACTION.replace('@urn@',group.urn).replace('@command@',group.command)},
+    body: SOAP_TEMPLATES.replace('@command@',group.command).replace('@action@',action)
   }, (err, res, body) => {
     if (err || res.statusCode !== 200) {
-      context.emit(':tell', 'No');
+      context.emit(':tell', 'Non');
       console.error(error);
       return;
     }
-    context.emit(':tell', 'Ok');
+    context.emit(':tell', 'D\'accord');
   });
 };
 
 var handlers = {
 
     "PowerIntent"() {
-      makeRequest(CODES.POWER, this);
+      makeRequest(COMMANDS.group,COMMANDS.power, this);
     },
 
-    "NetflixIntent"() {
+    /*"NetflixIntent"() {
       makeRequest(CODES.NETFLIX, this);
     },
 
     "AppsIntent"() {
       makeRequest(CODES.APPS, this);
-    },
+    },*/
 
     "MuteIntent"() {
-      makeRequest(CODES.MUTE, this);
+      makeRequest(COMMANDS.group,COMMANDS.volume.mute, this);
     },
 
     "VolUpIntent"() {
-      makeRequest(CODES.VOLUME_UP, this);
+      makeRequest(COMMANDS.group,COMMANDS.volume.up, this);
     },
 
     "VolDownIntent"() {
-      makeRequest(CODES.VOLUME_DOWN, this);
+      makeRequest(COMMANDS.group,COMMANDS.volume.down, this);
     },
 
-    "VolUpHighIntent"() {
+    /*"VolUpHighIntent"() {
       makeRequest(CODES.VOLUME_UP_HIGH, this);
     },
 
     "VolDownHighIntent"() {
       makeRequest(CODES.VOLUME_DOWN_HIGH, this);
-    },
+    },*/
 
-    "PlayIntent"() {
+    /*"PlayIntent"() {
       makeRequest(CODES.PLAY, this);
     },
 
     "PauseIntent"() {
       makeRequest(CODES.PAUSE, this);
-    },
+    },*/
 
     "AboutIntent": function () {
-        const speechOutput = `
-        TV remote was developed by Shaun Donnelly because he's too lazy
-        to look for the remote.`;
+        const speechOutput = `Régis fait le boulot d'amazon.`;
         this.emit(':tellWithCard', speechOutput, skillName, speechOutput);
     },
 
     "AMAZON.HelpIntent": function () {
         const speechOutput = `
-        Here are some things you can say:
-        Turn on the TV.
-        Turn on Netflix.
-        You can also say 'stop' if you're done.
-        So how can I help?`;
+        Voilà ce que vous pouvez dire:
+        Allume la TV.
+        Plus de volume TV.
+        Moins de volume TV`;
         this.emit(':ask', speechOutput, speechOutput);
     },
 
     "AMAZON.StopIntent": function () {
-        var speechOutput = "Goodbye";
-        this.emit(':tell', speechOutput);
+        this.emit(':tell', "Au revoir");
     },
 
     "AMAZON.CancelIntent": function () {
-        var speechOutput = "Goodbye";
-        this.emit(':tell', speechOutput);
+         this.emit(':tell', "Au revoir");
     },
 
     "LaunchRequest": function () {
       const speechText = `
-      Welcome to ${skillName}. For instructions on what you
-      to ask, say 'help me'.`;
-      const repromptText = `For instructions, say 'help me'.`
+      Bienvenue a ${skillName}. Pour savoir quoi demander, dites 'aide'.`;
+      const repromptText = `Pour les instructions, dites 'aide'.`
       this.emit(':ask', speechText, repromptText);
     }
 
